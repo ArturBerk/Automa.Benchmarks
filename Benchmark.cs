c5093a4c97db732c331bcdbde5e5ed14b0881a43
Berk <berkmail@mail.ru>
2018-06-14 17:29:03 +0300

    Added prepare methods

--- Benchmark.cs
+++ Benchmark.cs
@@ -10,13 +10,20 @@ namespace Automa.Benchmarks
     public abstract class Benchmark : IBenchmark
     {
         private readonly BenchmarkCase[] cases;
+        private readonly Dictionary<string, BenchmarkPrepare> prepares;
 
         public int IterationCount = 10;
 
         protected Benchmark()
         {
-            cases = SelectCases(GetType()
-                .GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)).ToArray();
+            var methods = GetType()
+                .GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).ToArray();
+            cases = SelectCases(methods).ToArray();
+            prepares = new Dictionary<string, BenchmarkPrepare>();
+            foreach (var benchmarkPrepare in SelectPrepares(methods))
+            {
+                prepares[benchmarkPrepare.Name] = benchmarkPrepare;
+            }
         }
 
         protected virtual void Prepare() { }
@@ -35,19 +42,37 @@ namespace Automa.Benchmarks
             }
         }
 
+        private IEnumerable<BenchmarkPrepare> SelectPrepares(IEnumerable<MethodInfo> methods)
+        {
+            foreach (var methodInfo in methods)
+            {
+                if (methodInfo.GetParameters().Length > 0) continue;
+                if (methodInfo.ReturnType != typeof(void)) continue;
+                var caseAttribute = methodInfo.GetCustomAttribute<CaseAttribute>();
+                if (caseAttribute == null) continue;
+                yield return new BenchmarkPrepare(
+                    caseAttribute.Name,
+                    (Action)Delegate.CreateDelegate(typeof(Action), this, methodInfo));
+            }
+        }
+
         public BenchmarkResult[] Execute()
         {
             Prepare();
             var iterationCount = IterationCount;
             Stopwatch stopwatch = new Stopwatch();
             BenchmarkResult[] results = new BenchmarkResult[cases.Length];
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-            GC.Collect();
             for (var index = 0; index < cases.Length; index++)
             {
-                stopwatch.Restart();
                 var benchmarkCase = cases[index];
+                if (prepares.TryGetValue(benchmarkCase.Name, out var prepare))
+                {
+                    prepare.Execute();
+                }
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+                stopwatch.Restart();
                 for (int i = 0; i < iterationCount; i++)
                 {
                     benchmarkCase.Execute();
@@ -115,12 +140,29 @@ namespace Automa.Benchmarks
                 Execute = execute;
             }
         }
+
+        private struct BenchmarkPrepare
+        {
+            public readonly string Name;
+            public readonly Action Execute;
+
+            public BenchmarkPrepare(string name, Action execute)
+            {
+                Name = name;
+                Execute = execute;
+            }
+        }
     }
 
     [AttributeUsage(AttributeTargets.Method)]
     public sealed class PrepareAttribute : Attribute
     {
+        public readonly string Name;
 
+        public PrepareAttribute(string name)
+        {
+            Name = name;
+        }
     }
 
     [AttributeUsage(AttributeTargets.Method)]
